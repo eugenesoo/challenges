fe8abd440a6f78e31e4052b6f6a9f8a41bb82401
Eugene Soo <eugene@soo.sg>
2017-11-28 22:11:17 -0800

    implemented optimised solution for checkPermutations

--- checkPermutation.js
+++ checkPermutation.js
@@ -1,7 +1,4 @@
 //brute force
--> get every possible combination of the first string
--> check if it matches the second string
-
 const getPermutations = (str) => {
   if (str.length === 1) {
     return [str];
@@ -33,3 +30,29 @@ const checkPermutationBad = (strA, strB) => {
 }
 
 //optimised
+const checkPermutations = (strA, strB) => {
+  // loop through strA and store all characters in an object
+  const strAChars = {};
+  for (let i = 0; i < strA.length; i += 1) {
+    strAChars[strA[i]] = true;
+  }
+  // loop through strB,
+  for (let i = 0; i < strB.length; i += 1) {
+    if (strAChars[strB[i]]) {
+      strAChars[strB[i]] = false;
+    } else {
+      return false;
+    }
+  }
+
+  return true;
+  //  if the character can be found in strA,
+  //  check the next X characters (based on the length of strA - 1) 
+  //  store current pointer;
+  //  store isvalidpermutation is true;
+  //  while current point is < current pointer + legnth of strA - 1 || isvalidpermutation is false,
+  //  if the character at current pointer cannot be found in the object, change is valid permutation to false;
+  //
+  //  at the end of the while loop, if isvalidpermutation is true, returnt rue
+  //  else continue loop through strB
+}
