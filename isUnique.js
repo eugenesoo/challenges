328d68ed3a36a194644c61e61820b503d14e2e0a
Eugene Soo <eugene@soo.sg>
2017-11-29 15:29:37 -0800

    complete isUnique in bitwise

--- isUnique.js
+++ isUnique.js
@@ -9,3 +9,28 @@ const isUnique = (string) => {
   }
   return true;
 }
+
+
+//using bitwise
+
+// assign lowercase characters a bit each
+const charBit = {};
+
+for (let i = 97; i < 123; i += 1) {
+  // get alphabet
+  const char = String.fromCharCode(i);
+  const intBit = Math.pow(2, i - 97);
+  charBit[char] = intBit;
+}
+
+const isUniqueBit = (string) => {
+  let checker = 0;
+  for (let i = 0; i < string.length; i += 1) {
+    if ((checker & charBit[string[i]]) === 0) {
+      checker = checker | charBit[string[i]];
+    } else {
+      return false;
+    }
+  }
+  return true;
+}
