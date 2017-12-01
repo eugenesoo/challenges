262b2cd9c8b815f7055e9878d19dbd1bce6c699b
Eugene Soo <eugene@soo.sg>
2017-11-30 16:50:59 -0800

    complete isPalindrome and isPalindromePerm

---
+++ isPalindrome.js
@@ -0,0 +1,52 @@
+const isPalindrome = (str) => {
+  let firstIndex = 0;
+  let lastIndex = str.length - 1;
+  while (firstIndex <= lastIndex) {
+    if (str[firstIndex].toLowerCase() !== str[lastIndex].toLowerCase()) {
+      return false;
+    }
+    firstIndex += 1;
+    lastIndex -= 1;
+  }
+  return true;
+}
+
+const isPalindromePerm = (str) => {
+  // pattern is that if str has odd chars
+  // must have 1 char with odd count, rest of chars must have even count
+  // if str has even chars
+  // chars must have even count
+  let oddCount = 0;
+  let evenCount = 0;
+  let charCount = 0;
+  const charObj = {};
+
+  for (let i = 0; i < str.length; i += 1) {
+    if (!charObj[str[i]] && str[i] !== ' ') {
+      charObj[str[i]] = 1;
+      oddCount += 1;
+    } else if (charObj[str[i]]){
+      charObj[str[i]] += 1;
+      charCount += 1;
+      if (charObj[str[i]] & 1) {
+        oddCount += 1;
+        evenCount -= 1;
+      } else {
+        oddCount -= 1;
+        evenCount += 1;
+      }
+    }
+  }
+
+  if (charCount & 1) {
+    if (oddCount !== 1) {
+      return false;
+    }
+    return true;
+  } else {
+    if (oddCount !== 0) {
+      return false;
+    }
+    return true;
+  }
+}
