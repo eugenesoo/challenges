397b30738f4d4501d141898d540c97fc828616fa
Eugene Soo <eugene@soo.sg>
2017-12-01 17:35:19 -0800

    complete altSumsHalf

--- altSums.js
+++ altSums.js
@@ -1,11 +1,24 @@
 const altSums = (intStr) => {
   let sum = 0;
   for (let i = 0; i < intStr.length; i += 1) {
-    if (intStr[i] === intStr[i +1]) {
+    if (intStr[i] === intStr[i + 1]) {
       sum += parseInt(intStr[i], 10);
     } else if (i === intStr.length - 1 && intStr[0] === intStr[i]) {
       sum += parseInt(intStr[i], 10);
     }
   }
   return sum;
+}
+
+const altSumsHalf = (intStr) => {
+  let sum = 0;
+  let intLength = intStr.length;
+  for (let i = 0; i < intLength / 2; i += 1) {
+    if (intStr[i] === intStr[i + intLength / 2]) {
+      sum += parseInt(intStr[i], 10);
+    } else if (i === intStr.length - 1 && intStr[0] === intStr[i]) {
+      sum += parseInt(intStr[i], 10);
+    }
+  }
+  return sum * 2;
 }
