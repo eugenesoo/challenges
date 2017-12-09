91a4092fd6d7afa675199b1fbdc3227c1f48a389
Eugene Soo <eugene@soo.sg>
2017-12-09 16:00:33 +0800

    complete fibonacci iteratively

--- nthFibonacci.js
+++ nthFibonacci.js
@@ -7,3 +7,24 @@ const nthFibonacciRecursive = (int) => {
   }
   return nthFibonacciRecursive(int - 1) + nthFibonacciRecursive(int - 2);
 }
+
+const nthfibonacciiterative = (int) => {
+  if (int === 0) {
+    return 0;
+  }
+  if (int === 1) {
+    return 1;
+  }
+  let n = 2;
+  let currentresult = 1;
+  let prevresult = 0;
+  let temp;
+  while (n <= int) {
+    temp = prevresult;
+    prevresult = currentresult;
+    currentresult += temp;
+    n++;
+  }
+  return currentresult;
+}
+
