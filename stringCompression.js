44bc10ff365a7ec679342f01fbd591412cd065ee
Eugene Soo <eugene@soo.sg>
2017-12-10 15:03:24 +0800

    complete stringCompression

--- stringCompression.js
+++ stringCompression.js
@@ -1,6 +1,5 @@
 const stringCompression = (str) => {
   const charHash = {};
-  let compressedStr = '';
   for (let i = 0; i < str.length; i += 1) {
     if (charHash[str[i]]) {
       charHash[str[i]] += 1;
@@ -13,4 +12,40 @@ const stringCompression = (str) => {
     return str;
   }, '');
   return compressedStr.length < str.length ? compressedStr : str;
+}
+
+const stringCompressionOptimised = (str) => {
+  if (checkCompressedStrLength > str.length) {
+    return str;
+  }
+
+  let compressedStr = '';
+  let currentChar;
+  let currentCharCount = 0;
+  for (let i = 0; i < str.length; i += 1) {
+    if (!currentChar) {
+      currentChar = str[i];
+      currentCharCount = 1;
+    } else if (str[i] === currentChar) {
+      currentCharCount += 1;
+    } else {
+      compressedStr = compressedStr + currentChar + currentCharCount;
+      currentChar = str[i];
+      currentCharCount = 1;
+    }
+  }
+  compressedStr = compressedStr + currentChar + currentCharCount;
+  return compressedStr;
+}
+
+const checkCompressedStrLength = (str) => {
+  let length = 0;
+  let currentChar = str[0];
+  for (let i = 1; i < str.length; i += 1) {
+    if (str[i] !== currentChar) {
+      length += 2;
+      currentChar = str[i];
+    }
+  }
+  return length + 2;
 }
