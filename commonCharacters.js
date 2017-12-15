6308116e065b67ed298ffffd51ddf34b4e354b93
Eugene Soo <20952179+eugenesoo@users.noreply.github.com>
2017-12-15 07:47:14 -0800

    complete commonChars

---
+++ commonCharacters.js
@@ -0,0 +1,16 @@
+const commonCharacter = (str1, str2) => {
+  const hash = {};
+  let str = '';
+  for (let i = 0; i < str2.length; i =+ 1) {
+    hash[str2[i]] = true; 
+  }
+  for (let i = 0; i < str1.length; i += 1) {
+    if (hash[str1[i]]) {
+      str += str1[i];
+      hash[str[i]] = false;
+    }
+  }
+  return str;
+}
+
+console.log(commonCharacter('acexivou', 'aegihobu'));
